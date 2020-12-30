blue-nebula/base

commit a9a9ef06d262431863a15c41c74432782f67e8b0

Revert "parkour physics tweak"
This reverts commit 3027c0244ac6aa604477d1d06f319a4296e90b3e.

This fixes #151

I'm reverting this commit because it causes the bug described in the
issue and reverting it fixes the bug.  The bug is fixed by making sure
we send the player's velocity to the `collide` function.  Inside the
collision code, the player's velocity is used to ignore collisions with
cube faces if the player is too deep inside the cube, as we can see in
this code line, part of definition of macro CHECKSIDE inside
engine/physics.cpp:

    if(d->type<ENT_CAMERA && dotval < 0 && dist < margin) continue; \

That code line is only executed if `dir` (the velocity of the player) is
non-zero, which this change makes sure that it will be.

Another possible fix could be to move the code line above out from the
if statement that checks if `dir` is non-zero, and also remove `dotval <
0` since `dotval` will be 0 if `dir` is 0, but the way I solve it in
this commit feels less risky.

src/game/physics.cpp
@@ -855,7 +855,7 @@ namespace physics
                 if(!move && !strafe) continue;
                 vecfromyawpitch(d->yaw, 0, move, strafe, dir);
                 d->o.add(dir);
-                bool collided = collide(d);
+                bool collided = collide(d, dir);
                 d->o = oldpos;
                 if(!collided || hitplayer || collidewall.iszero()) continue;
                 vec face = vec(collidewall).normalize();
@@ -875,16 +875,16 @@ namespace physics
                         if(onfloor)
                         {
                             d->o.z += space*m;
-                            if(collide(d))
+                            if(collide(d, dir))
                             {
                                 d->o.z += space*n-space*m;
-                                if(!collide(d) || hitplayer) vault = true;
+                                if(!collide(d, dir) || hitplayer) vault = true;
                             }
                         }
                         else
                         {
                             d->o.z += space*n;
-                            if(!collide(d) || hitplayer) vault = true;
+                            if(!collide(d, dir) || hitplayer) vault = true;
                         }
                         d->o = oldpos;
                     }

robalni/fragility

commit 66f514a745782bc7138a9576e62f1c27a6c3d4bb

Fix wall-kick collision
Wall-kicks don't always work when done close to a ceiling. This bug was
introduced in commit:

a9a9ef06d262431863a15c41c74432782f67e8b0 Revert "parkour physics tweak"

This commit partially reverts that commit so that wall-kicks work again
but wall-running still doesn't have the bug that was fixed in that
commit.

src/game/physics.cpp
@@ -873,16 +873,16 @@ namespace physics
                         if(onfloor)
                         {
                             d->o.z += space*m;
-                            if(collide(d, dir))
+                            if(collide(d))
                             {
                                 d->o.z += space*n-space*m;
-                                if(!collide(d, dir) || hitplayer) vault = true;
+                                if(!collide(d) || hitplayer) vault = true;
                             }
                         }
                         else
                         {
                             d->o.z += space*n;
-                            if(!collide(d, dir) || hitplayer) vault = true;
+                            if(!collide(d) || hitplayer) vault = true;
                         }
                         d->o = oldpos;
                     }